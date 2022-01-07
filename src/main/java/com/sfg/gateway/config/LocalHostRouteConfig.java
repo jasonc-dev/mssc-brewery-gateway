92d970111fa3496402f49c6641ba2d9ab12ff5a8
jasonc-dev <jasoncohen07@gmail.com>
2022-01-07 21:09:55 +0000

    adding route for orders

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -12,8 +12,11 @@ public class LocalHostRouteConfig {
     public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                .uri("http://localhost:8080")
-                .id("beer-service"))
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
                 .build();
     }
 }
